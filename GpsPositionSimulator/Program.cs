054acd3a2431be280a3f09ac622ba719ce9c53da
ddemcak <ddemcak@gmail.com>
2019-12-17 20:47:20 +0100

    Code clean-up, removed unnecessary code, added input arguments validation.

--- GpsPositionSimulator/Program.cs
+++ GpsPositionSimulator/Program.cs
@@ -14,83 +14,111 @@ namespace GpsPositionSimulator
     class Program
     {
 
-        //private const string GgaMessage1 = "$GPGGA,091317.80,5037.0968,N,00534.6232,E,9,07,1.4,73.65,M,46.60,M,05,0136*50";
-
-        //{"quality": 1, "timestamp_us": 0, "timestamp": 1525421184, "altitude": 156.59999999999999, "precision": 0.73999999999999999, "longitude": 1424.4403500000001, "latitude": 5003.7251999999999, "satellites": 12, "speed": 0.109}
+        static FileInfo inputFile = null;
+        static FileInfo outputFile = null;
+        static int timeOffset = 0;
+        static int speedMultiplier = 0;
 
 
         static void Main(string[] args)
         {
 
-            if (args.Length != 3)
+            if (args.Length != 4)
             {
-                Console.WriteLine("Please provide an input, an output file and offset!");
+                Console.WriteLine(@"Please provide an input arguments as follows:
+1st - Input file with NMEA data,
+2nd - Output JSON file path,
+3rd - Time offset in seconds,
+4th - Speed multipler constant.");
                 return;
             }
 
-            // Original data simulation
-            if (args[0] == "s")
+            if (!ValidateInputArguments(args))
             {
-                int count = 0;
-                while (count < 100)
-                {
-                    StreamWriter sw = new StreamWriter(args[1]);
-                    sw.WriteLine(SimulatedNmeaSenteceToJson());
+                return;
+            }
 
-                    Console.WriteLine(SimulatedNmeaSenteceToJson());
+
+            StreamReader sr = new StreamReader(inputFile.FullName);
+            List<NmeaMessage> messages = new List<NmeaMessage>();
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+
+                if (line.Contains("$GPGGA") || line.Contains("$GPRMC"))
+                {
+                    var parser = new NmeaParser();
+                    messages.Add(parser.Parse(line));
+                }
+
+                if (messages.Count == 2)
+                {
+                    string message = NmeaSenteceToJson(messages, timeOffset, speedMultiplier);
+
+                    StreamWriter sw = new StreamWriter(outputFile.FullName);
+                    sw.WriteLine(message);
+
+                    Console.WriteLine(message);
 
                     sw.Close();
 
-                    count++;
+                    messages.Clear();
                     Thread.Sleep(1000);
                 }
+
+
             }
-            // Simulated from file data
-            else
-            {
-                StreamReader sr = new StreamReader(args[0]);
-                List<NmeaMessage> messages = new List<NmeaMessage>();
-
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-
-
-
-                    if (line.Contains("$GPGGA") || line.Contains("$GPRMC"))
-                    {
-                        var parser = new NmeaParser();
-                        messages.Add(parser.Parse(line));
-                    }
-
-                    if (messages.Count == 2)
-                    {
-                        string message = NmeaSenteceToJson(messages, int.Parse(args[2]));
-
-                        StreamWriter sw = new StreamWriter(args[1]);
-                        sw.WriteLine(message);
-
-                        Console.WriteLine(message);
-
-                        sw.Close();
-
-                        messages.Clear();
-                        Thread.Sleep(1000);
-                    }
-
-
-                }
-                sr.Close();
-            }
-
-
-
-
-            //Console.ReadLine();
+            sr.Close();
 
         }
 
-        private static string NmeaSenteceToJson(List<NmeaMessage> messages, int offsetInSeconds)
+        public static bool ValidateInputArguments(string[] args)
+        {
+            try
+            {
+                inputFile = new FileInfo(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("Input argument {0} is not valid!", args[0]));
+                return false;
+            }
+
+            try
+            {
+                outputFile = new FileInfo(args[1]);
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("Input argument {0} is not valid!", args[1]));
+                return false;
+            }
+
+            try
+            {
+                timeOffset = int.Parse(args[2]);
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("Input argument {0} is not valid!", args[2]));
+                return false;
+            }
+
+            try
+            {
+                speedMultiplier = int.Parse(args[3]);
+            }
+            catch
+            {
+                Console.WriteLine(string.Format("Input argument {0} is not valid!", args[3]));
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string NmeaSenteceToJson(List<NmeaMessage> messages, int offsetInSeconds, int speedMultipler)
         {
             if (messages.Count != 2) return null;
 
@@ -102,16 +130,7 @@ namespace GpsPositionSimulator
             // Longitude and Latitude needs to by formatted in the same way as for the project
             // Speed is in knots 1 knt = 1.852 km/h
             return string.Format("{{\"quality\": 1, \"timestamp_us\": 0, \"timestamp\": {0}, \"altitude\": {1}, \"precision\": {2}, \"longitude\": {3}, \"latitude\": {4}, \"satellites\": {5}, \"speed\": {6}}}",
-                unixTimestamp, gpgga.Altitude, gpgga.Hdop, gpgga.Longitude * 100, gpgga.Latitude * 100, gpgga.NumberOfSatellites, rmc.Speed * 1.852);
-        }
-
-        private static string SimulatedNmeaSenteceToJson()
-        {
-            //{"quality": 1, "timestamp_us": 0, "timestamp": 1525421184, "altitude": 156.59999999999999, "precision": 0.73999999999999999, "longitude": 1424.4403500000001, "latitude": 5003.7251999999999, "satellites": 12, "speed": 0.109}
-            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-
-            return string.Format("{{\"quality\": 1, \"timestamp_us\": 0, \"timestamp\": {0}, \"altitude\": 156.59999999999999, \"precision\": 0.73999999999999999, \"longitude\": 1424.4403500000001, \"latitude\": 5003.7251999999999, \"satellites\": 12, \"speed\": 0.109}}",
-                unixTimestamp);
+                unixTimestamp, gpgga.Altitude, gpgga.Hdop, gpgga.Longitude * 100, gpgga.Latitude * 100, gpgga.NumberOfSatellites, rmc.Speed * 1.852 * speedMultipler);
         }
     }
 }
